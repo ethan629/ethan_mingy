c7165873a30c9c752c084bc3137fab6ec5f13f9c
ethan629 <expan09@gmail.com>
2022-06-12 21:43:39 -0700

    0.0.2

--- src/main/java/net/ethan/tutorialmod/item/ModItems.java
+++ src/main/java/net/ethan/tutorialmod/item/ModItems.java
@@ -4,10 +4,12 @@ import net.ethan.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+    // raw materials
     public static final Item GARNET = registerItem("garnet",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
@@ -24,6 +26,27 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
 
+    // carrots
+    public static final Item GARNET_CARROT = registerItem("garnet_carrot",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.GARNET_CARROT)));
+
+    public static final Item RAINBOW_OPAL_CARROT = registerItem("rainbow_opal_carrot",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.RAINBOW_OPAL_CARROT)));
+
+    public static final Item SAPPHIRE_CARROT = registerItem("sapphire_carrot",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.SAPPHIRE_CARROT)));
+
+    public static final Item TOPAZ_CARROT = registerItem("topaz_carrot",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.TOPAZ_CARROT)));
+
+    public static final Item TOURMALINE_CARROT = registerItem("tourmaline_carrot",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.TOURMALINE_CARROT)));
+
+
+    // tools
+    public static final Item GARNET_SWORD = registerItem("garnet_sword",
+            new SwordItem(ModToolMaterials.GARNET, 1, 2f,
+                    new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
